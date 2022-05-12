41f7128485f3400868d16e3820d32007f8ead7d4
Henrique Vieira <henrique@mvstudio.be>
2022-05-12 20:20:02 +0200

    wip

--- js/sections/_menu.js
+++ js/sections/_menu.js
@@ -1,12 +1,24 @@
+/* SMALL MENU ON SCROLL */
+
 const header = document.getElementById("header");
 
 window.onscroll = function () {
-  if (
-    document.body.scrollTop > 130 ||
-    document.documentElement.scrollTop > 130
-  ) {
-    header.classList.add("small");
-  } else {
-    header.classList.remove("small");
-  }
+    if (document.body.scrollTop > 130 || document.documentElement.scrollTop > 130) {
+        header.classList.add("small");
+    } else {
+        header.classList.remove("small");
+    }
 };
+
+/* NO SCROLL */
+
+const toggle = document.getElementById("toggle");
+const body = document.querySelector("body");
+
+toggle.addEventListener("change", () => {
+    if (toggle.checked) {
+        body.classList.add("noscroll");
+    } else {
+        body.classList.remove("noscroll");
+    }
+});
